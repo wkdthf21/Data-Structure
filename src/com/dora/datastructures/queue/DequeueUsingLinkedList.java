0a3148aa7bf88dd0640e10d33d0aa8a29fce88ff
wkdthf21 <wkdthf21@naver.com>
2020-10-21 21:55:09 +0900

    Dequeue

---
+++ src/com/dora/datastructures/queue/DequeueUsingLinkedList.java
@@ -0,0 +1,141 @@
+package com.dora.datastructures.queue;
+
+/**
+ * 
+ * Dequeue Using Doubly LinkedList 
+ * 
+ * @author yesol
+ *
+ * @param <T>
+ */
+public class DequeueUsingLinkedList<T> {
+	
+	
+	private static class QueueNode<T>{
+		
+		T data;
+		QueueNode<T> prev;
+		QueueNode<T> next;
+		
+		public QueueNode(T data, QueueNode<T> prev, QueueNode<T> next){
+			this.data = data;
+			this.prev = prev;
+			this.next = next;
+		}
+		
+	}
+	
+	int size;
+	QueueNode<T> front;
+	QueueNode<T> rear;
+	
+	public DequeueUsingLinkedList() {
+		size = 0;
+		front = null;
+		rear = null;
+	}
+	
+	/**
+	 * 
+	 * insert element into the first position of LinkedList
+	 * 
+	 * @param data
+	 */
+	public void insertFront(T data) {
+		
+		QueueNode<T> node = new QueueNode<>(data, null, null);
+		
+		if(isEmpty()) {
+			front = node;
+			rear = node;
+		}
+		else {
+			node.next = front;
+			front.prev = node;
+			front = node;
+		}
+		
+		++size;
+		
+	}
+
+	/**
+	 * 
+	 * insert element into the last position of LinkedList
+	 * 
+	 * @param data
+	 */
+	public void insertRear(T data) {
+		QueueNode<T> node = new QueueNode<>(data, null, null);
+		
+		if(isEmpty()) {
+			front = node;
+			rear = node;
+		}
+		else {
+			node.prev = rear;
+			rear.next = node;
+			rear = node;
+		}
+		
+		++size;
+		
+	}
+	
+	/**
+	 * 
+	 * pop element from the first position of linked list
+	 * 
+	 * @return
+	 */
+	public T popFront() {
+		
+		if(isEmpty()) throw new RuntimeException("Dequeue is Empty");
+		
+		QueueNode<T> node = front;
+		front = front.next;
+		if(front == null) rear = null;
+		else {
+			front.prev.next = null;
+			front.prev = null;
+		}
+		
+		--size;
+	
+		return node.data;
+	}
+	
+	/**
+	 * 
+	 * pop element from the last position of linked list
+	 * 
+	 * @return
+	 */
+	public T popRear() {
+		
+		if(isEmpty()) throw new RuntimeException("Dequeue is Empty");
+		
+		QueueNode<T> node = rear;
+		rear = rear.prev;
+		if(rear == null) front = null;
+		else {
+			rear.next.prev = null;
+			rear.next = null;
+		}
+		
+		--size;
+		
+		return node.data;
+	}
+	
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	
+}
---
+++ src/com/dora/test_datastructures/queue/DequeueUsingLinkedListTest.java
@@ -0,0 +1,58 @@
+package com.dora.test_datastructures.queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.dora.datastructures.queue.DequeueUsingLinkedList;
+
+class DequeueUsingLinkedListTest {
+
+	@Test
+	void createEmptyDequeue() {
+		DequeueUsingLinkedList<Integer> dequeue = new DequeueUsingLinkedList<>();
+		assertTrue(dequeue.isEmpty());
+	}
+	
+	@Test
+	void testInsertFront() {
+		
+		DequeueUsingLinkedList<Integer> dequeue = new DequeueUsingLinkedList<>();
+		
+		for(int i = 0; i < 10; i++) dequeue.insertFront(i);
+		
+		assertEquals(dequeue.getSize(), 10);
+		for(int i = 0; i < 10; i++) assertEquals(dequeue.popRear(), i);
+		assertTrue(dequeue.isEmpty());
+		
+	}
+	
+	@Test
+	void testInsertRear() {
+		
+		DequeueUsingLinkedList<Integer> dequeue = new DequeueUsingLinkedList<>();
+		
+		for(int i = 0; i < 10; i++) dequeue.insertRear(i);
+		
+		assertEquals(dequeue.getSize(), 10);
+		for(int i = 0; i < 10; i++) assertEquals(dequeue.popFront(), i);
+		assertTrue(dequeue.isEmpty());
+	}
+	
+	
+	@Test
+	void popEmptyDequeue() {
+		
+		DequeueUsingLinkedList<Integer> dequeue = new DequeueUsingLinkedList<>();
+		
+		for(int i = 0; i < 10; i++) dequeue.insertFront(i);
+		
+		for(int i = 0; i < 10; i++) dequeue.popFront();
+		
+		assertThrows(RuntimeException.class, () -> {
+			dequeue.popFront();
+		});
+		
+	}
+
+}
